a09ce597f97fa00c6a973f626c4f595462ef1be1
Ari Ushiki <ariana.ushiki@gmail.com>
2019-02-26 08:30:56 -0500

    layered funal

---
+++ ci/project2/future/assets/js/scripts.js
@@ -0,0 +1,63 @@
+function showImage1() {
+  var image = document.querySelector(".futureimg3");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#futuretext1");
+el.addEventListener("click", showImage1, false);
+
+function showImage2() {
+  var image = document.querySelector(".futureimg6");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#futuretext2");
+el.addEventListener("click", showImage2, false);
+
+function showImage3() {
+  var image = document.querySelector(".futureimg5");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#futuretext3");
+el.addEventListener("click", showImage3, false);
+
+function showImage4() {
+  var image = document.querySelector(".futureimg2");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#futuretext4");
+el.addEventListener("click", showImage4, false);
+
+function showImage5() {
+  var image = document.querySelector(".futureimg4");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#futuretext5");
+el.addEventListener("click", showImage5, false);
+
+function togglediv1(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv2(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv3(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv4(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv5(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+function togglediv6(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
---
+++ ci/project2/past/assets/js/scripts.js
@@ -0,0 +1,56 @@
+function showImage1() {
+  var image = document.querySelector(".pastimg5");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#pasttext1");
+el.addEventListener("click", showImage1, false);
+
+function showImage2() {
+  var image = document.querySelector(".pastimg4");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#pasttext2");
+el.addEventListener("click", showImage2, false);
+
+function showImage3() {
+  var image = document.querySelector(".pastimg3");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#pasttext3");
+el.addEventListener("click", showImage3, false);
+
+function showImage4() {
+  var image = document.querySelector(".pastimg2");
+  image.style["display"] = "block";
+}
+var el = document.querySelector("#pasttext4");
+el.addEventListener("click", showImage4, false);
+
+function togglediv1(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv2(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv3(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv4(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function togglediv5(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
+function togglediv6(id) {
+  var div = document.getElementById(id);
+  div.style.display = div.style.display == "none" ? "block" : "none";
+}
